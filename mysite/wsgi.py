44dc7fe79188ab28c971c5b61e011e984101e7f6
asmin <asminalev@gmail.com>
2018-08-04 00:31:49 +0300

    change wsg

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -7,10 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
-import os
+import os, django
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
-application = get_wsgi_application()
+application = django.core.handlers.wsgi.WSGIHandler()
